0f8ceac139c98bcc25f7ed79eccd30996b325403
hilman khitam <hilmankhitam100@gmail.com>
2023-10-14 15:31:19 +0800

    update eslint

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,7 +1,8 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next"
 import { getSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context);
@@ -24,12 +25,16 @@ const profile = () => {
     const router = useRouter();
     const { data: user } = useCurrentUser();
 
+    const selectProfile = useCallback(() => {
+        router.push('/');
+    }, [router]);
+
     return (
         <div className="flex items-center h-full justify-center">
             <div className="flex flex-col">
                 <h1 className="text-3xl md:text-6xl text-white text-center">Who is watching?</h1>
                 <div className="flex items-center justify-center gap-8 mt-10">
-                    <div onClick={() => { router.push('/'); }}>
+                    <div onClick={() => selectProfile()}>
                         <div className="group flex-row w-44 mx-auto">
                             <div className="
                                 w-44
